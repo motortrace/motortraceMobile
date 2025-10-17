db44329ab313eaf97f24deeb9df72a497e969318
SadheeyaM <sadheeyasalim10@gmail.com>
2025-10-17 18:47:21 +0530

    Layout change

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthProvider, useAuth } from "./src/context/AuthContext";
 
 // Screens
 import TechnicianHome from "./src/screens/Technician/TechnicianHome";
@@ -10,54 +11,40 @@ import InspectionListScreen from "./src/screens/Technician/InspectionList";
 import InspectionDetailsScreen from "./src/screens/Technician/InspectionReport";
 import InspectionReportScreen from "./src/screens/Technician/InspectionReport";
 import ProfileScreen from "./src/screens/Technician/Profile";
+import LoginScreen from "./src/screens/Technician/Login";
 
 const Stack = createNativeStackNavigator();
 
+function RootNavigator() {
+  const { user, loading } = useAuth();
+
+  if (loading) return null; // optional: show splash screen
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={TechnicianHome} />
+          <Stack.Screen name="WorkOrderList" component={WorkOrderListScreen} />
+          <Stack.Screen name="WorkOrderDetails" component={WorkOrderDetailsScreen} />
+          <Stack.Screen name="InspectionList" component={InspectionListScreen} />
+          <Stack.Screen name="InspectionDetails" component={InspectionDetailsScreen} />
+          <Stack.Screen name="InspectionReport" component={InspectionReportScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        {/* Home */}
-        <Stack.Screen
-          name="Home"
-          component={TechnicianHome}
-          options={{ headerShown: false }}
-        />
-
-        {/* Work Orders */}
-        <Stack.Screen
-          name="WorkOrderList"
-          component={WorkOrderListScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="WorkOrderDetails"
-          component={WorkOrderDetailsScreen}
-          options={{ headerShown: false }}
-        />
-
-        {/* Inspections */}
-        <Stack.Screen
-          name="InspectionList"
-          component={InspectionListScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="InspectionDetails"
-          component={InspectionDetailsScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="InspectionReport"
-          component={InspectionReportScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
